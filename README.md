49a2a17855da5b3747b95ba8c3a3756d2c4ae6b8
RYuriy <radov.yuriy@ukr.net>
2021-08-20 02:35:49 +0300

    + Entity Framework Core setup

--- README.md
+++ README.md
@@ -7,8 +7,24 @@ dotnet new globaljson --sdk-version 5.0.302 --output SportsSln/SportsStore
 dotnet new web --no-https --output SportsSln/SportsStore --framework net5.0
 dotnet new sln -o SportsSln
 dotnet sln SportsSln add SportsSln/SportsStore
+
 ### Tests Project
 dotnet new xunit -o SportsSln/SportsStore.Tests --framework net5.0
 dotnet sln SportsSln add SportsSln/SportsStore.Tests
 dotnet add SportsSln/SportsStore.Tests reference SportsSln/SportsStore
 dotnet add SportsSln/SportsStore.Tests package Moq --version 4.16.1
+
+### Entity Framework Core
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 5.0.9
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 5.0.9
+
+dotnet tool uninstall --global dotnet-ef
+dotnet tool install --global dotnet-ef --version 5.0.9
+
+#### EF Commands
+cd .\SportsStore\
+dotnet ef migrations add Initial
+
+
+
+
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -5,18 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SportsStore.Models;
 
 namespace SportsStore
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts => {
+                opts.UseSqlServer(Configuration["ConnectionStrings:SportsStoreConnection"]);
+            });
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
