8488f6bd7c531ddd475c23e67ca55c0017e7a0d8
RYuriy <radov.yuriy@ukr.net>
2021-08-29 19:04:08 +0300

    + update page count accordingly category

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -30,7 +30,9 @@ namespace SportsStore.Controllers
                 {
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = category == null ? 
+                    repository.Products.Count() :
+                    repository.Products.Where(e => e.Category == category).Count()
                 },
                 CurrentCategory = category
             });
