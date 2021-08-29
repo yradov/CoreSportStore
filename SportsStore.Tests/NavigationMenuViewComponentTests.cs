d69ca5da2ffb0c5940791d1f9de8922f4ec043f3
RYuriy <radov.yuriy@ukr.net>
2021-08-29 18:42:18 +0300

    + Navigation Menu View Component

---
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using SportsStore.Components;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+
+namespace SportsStore.Tests
+{
+    public class NavigationMenuViewComponentTests
+    {
+        [Fact]
+        public void Can_Select_Categories()
+        {
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product{ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product{ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product{ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product{ProductID = 4, Name = "P4", Category = "Orages"},
+            }).AsQueryable<Product>());
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+
+            string[] result = ((IEnumerable<string>)(target.Invoke() as ViewViewComponentResult)
+                .ViewData.Model).ToArray();
+
+            Assert.True(Enumerable.SequenceEqual(new string[] { "Apples", "Orages", "Plums" }, result));
+        }
+
+        [Fact]
+        public void Indicate_Selected_Category()
+        {
+            string categoryToSelect = "Apples";
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product{ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product{ProductID = 4, Name = "P2", Category = "Orages"},
+            }).AsQueryable<Product>());
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+
+            target.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext
+                {
+                    RouteData = new Microsoft.AspNetCore.Routing.RouteData()
+                }
+            };
+            target.RouteData.Values["category"] = categoryToSelect;
+
+            string result = (string)(target.Invoke() as ViewViewComponentResult).ViewData["SelectedCategory"];
+
+            Assert.Equal(categoryToSelect, result);
+        }
+    }
+}
