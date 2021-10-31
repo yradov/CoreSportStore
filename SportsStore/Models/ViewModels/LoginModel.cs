24ec579fc390a901a68de7e924f61d65deaa9081
RYuriy <radov.yuriy@ukr.net>
2021-10-31 14:59:46 +0200

    + all security

---
+++ SportsStore/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
